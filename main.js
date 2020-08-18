09cd5ed4074f80ebc1002f70334aea1c15314028
David Turner <csusbdt@gmail.com>
2020-08-17 17:46:56 -1000

    X

--- main.js
+++ main.js
@@ -46,11 +46,12 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+console.log(message.author.id);
+console.log(message.type);
   if (typeof message.recipient === 'undefined') return;
+console.log(message.recipient.id);
   if (message.recipient.id != botId) return;
   if (message.author.id        == botId      ) return;
-console.log(message.author.id);
-console.log(message.recipient.id);
 
 /*
   if (message.author.bot                     ) return; // to avoid infinite recursion
